231e7a777f8c556efe924c680ec1b4adc2b710cf
Erkan Öztürk <43139925+erkanozturk1903@users.noreply.github.com>
2024-10-11 07:37:24 +0300

    v3

--- yes_no_app/lib/presentation/widgets/chat/her_messge_bubble.dart
+++ yes_no_app/lib/presentation/widgets/chat/her_messge_bubble.dart
@@ -40,11 +40,25 @@ class HerMessageBubble extends StatelessWidget {
 class _ImageBubble extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context);
+    final size = MediaQuery.of(context).size;
+
     return ClipRRect(
       borderRadius: BorderRadius.circular(20),
       child: Image.network(
         'https://yesno.wtf/assets/no/31-cc391a4c0332a4ce5b4d4666f64a7b4a.gif',
+        width: size.width * 0.7,
+        height: 150,
+        fit: BoxFit.cover,
+        loadingBuilder: (context, child, loadingProgress) {
+          if (loadingProgress == null) return child;
+
+          return Container(
+            width: size.width * 0.7,
+            height: 150,
+            padding:const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+            child: const Text('Mira bir resim gönderiyor'),
+          );
+        },
       ),
     );
   }
---
+++ yes_no_app/lib/presentation/widgets/shared/message_field_box.dart
@@ -0,0 +1,46 @@
+// ignore_for_file: avoid_print
+
+import 'package:flutter/material.dart';
+
+class MessageFieldBox extends StatelessWidget {
+  const MessageFieldBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final textController = TextEditingController();
+    final focusNode = FocusNode();
+
+    final outlineInputBorder = UnderlineInputBorder(
+        borderSide: const BorderSide(color: Colors.transparent),
+        borderRadius: BorderRadius.circular(40));
+
+    final inputDecoration = InputDecoration(
+      hintText: 'Mesajınızı Giriniz',
+      enabledBorder: outlineInputBorder,
+      focusedBorder: outlineInputBorder,
+      filled: true,
+      suffixIcon: IconButton(
+        onPressed: () {
+          final textValue = textController.value.text;
+          print('Metin kutusu değeri? $textValue');
+          textController.clear();
+        },
+        icon: const Icon(Icons.send_outlined),
+      ),
+    );
+
+    return TextFormField(
+      onTapOutside: (event) {
+        focusNode.unfocus();
+      },
+      focusNode: focusNode,
+      controller: textController,
+      decoration: inputDecoration,
+      onFieldSubmitted: (value) {
+        print('Değer gönderildi $value');
+        textController.clear();
+        focusNode.requestFocus();
+      },
+    );
+  }
+}
