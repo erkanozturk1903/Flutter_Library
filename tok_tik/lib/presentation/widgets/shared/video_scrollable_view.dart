3ae29db111c98923f1acd5311a3e6c6aca12f6cd
Erkan Öztürk <43139925+erkanozturk1903@users.noreply.github.com>
2024-10-13 14:33:01 +0300

    v7

--- tok_tik/lib/presentation/widgets/shared/video_scrollable_view.dart
+++ tok_tik/lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tok_tik/domain/entities/video_post.dart';
+import 'package:tok_tik/presentation/widgets/shared/video_button.dart';
+import 'package:tok_tik/presentation/widgets/video/fullscreen_player.dart';
 
 class VideoScrollableView extends StatelessWidget {
   final List<VideoPost> videos;
@@ -7,29 +9,33 @@ class VideoScrollableView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PageView(
+    return PageView.builder(
       scrollDirection: Axis.vertical,
       physics: const BouncingScrollPhysics(),
-      children: [
-        Container(
-          color: Colors.red,
-        ),
-        Container(
-          color: Colors.blue,
-        ),
-        Container(
-          color: Colors.teal,
-        ),
-        Container(
-          color: Colors.yellow,
-        ),
-        Container(
-          color: Colors.pink,
-        ),
-        Container(
-          color: Colors.deepPurple,
-        )
-      ],
+      itemCount: videos.length,
+      itemBuilder: (context, index) {
+        final VideoPost videoPost = videos[index];
+        return Stack(
+          children: [
+            //VideoPlayer
+            SizedBox.expand(
+                child: FullScreenPlayer(
+              caption: videoPost.caption,
+              videoUrl: videoPost.videoUrl,
+            )),
+            // FullScreenPlayer(video: videoPost),
+
+            // Button
+            Positioned(
+              bottom: 40,
+              right: 20,
+              child: VideoButton(
+                video: videoPost,
+              ),
+            )
+          ],
+        );
+      },
     );
   }
 }
