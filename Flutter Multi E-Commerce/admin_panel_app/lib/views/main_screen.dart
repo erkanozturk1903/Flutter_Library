1cf714c8ae01a1f6b5f22a45c969ef0ce7e5d2ed
Erkan Öztürk <43139925+erkanozturk1903@users.noreply.github.com>
2024-11-10 15:40:45 +0300

    v28

---
+++ Flutter Multi E-Commerce/admin_panel_app/lib/views/main_screen.dart
@@ -0,0 +1,129 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:admin_panel_app/views/side_bar_screens/buyers_screen.dart';
+import 'package:admin_panel_app/views/side_bar_screens/category_screen.dart';
+import 'package:admin_panel_app/views/side_bar_screens/orders_screen.dart';
+import 'package:admin_panel_app/views/side_bar_screens/products_screen.dart';
+import 'package:admin_panel_app/views/side_bar_screens/upload_banner_screen.dart';
+import 'package:admin_panel_app/views/side_bar_screens/vendors_screen.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_admin_scaffold/admin_scaffold.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  Widget _selectedScreen = CategoryScreen();
+
+  screenSelector(item) {
+    switch (item.route) {
+      case BuyersScreen.id:
+        setState(() {
+          _selectedScreen = BuyersScreen();
+        });
+        break;
+      case VendorsScreen.id:
+        setState(() {
+          _selectedScreen = VendorsScreen();
+        });
+        break;
+      case OrdersScreen.id:
+        setState(() {
+          _selectedScreen = OrdersScreen();
+        });
+        break;
+      case CategoryScreen.id:
+        setState(() {
+          _selectedScreen = CategoryScreen();
+        });
+        break;
+      case UploadBannerScreen.id:
+        setState(() {
+          _selectedScreen = UploadBannerScreen();
+        });
+        break;
+      case ProductsScreen.id:
+        setState(() {
+          _selectedScreen = ProductsScreen();
+        });
+        break;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AdminScaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text(
+          'Management',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: _selectedScreen,
+      sideBar: SideBar(
+        header: Container(
+          height: 50,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: Colors.black,
+          ),
+          child: Center(
+            child: Text(
+              'Multi Vendor Admin',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 1.7,
+              ),
+            ),
+          ),
+        ),
+        items: const [
+          AdminMenuItem(
+            title: 'Vendors',
+            route: VendorsScreen.id,
+            icon: CupertinoIcons.person_3,
+          ),
+          AdminMenuItem(
+            title: 'Buyers',
+            route: BuyersScreen.id,
+            icon: CupertinoIcons.person,
+          ),
+          AdminMenuItem(
+            title: 'Orders',
+            route: OrdersScreen.id,
+            icon: CupertinoIcons.shopping_cart,
+          ),
+          AdminMenuItem(
+            title: 'Categories',
+            route: CategoryScreen.id,
+            icon: Icons.category,
+          ),
+          AdminMenuItem(
+            title: 'Upload Banners',
+            route: UploadBannerScreen.id,
+            icon: Icons.upload,
+          ),
+          AdminMenuItem(
+            title: 'Product',
+            route: ProductsScreen.id,
+            icon: Icons.store,
+          ),
+        ],
+        selectedRoute: VendorsScreen.id,
+        onSelected: (item) {
+          screenSelector(item);
+        },
+      ),
+    );
+  }
+}
